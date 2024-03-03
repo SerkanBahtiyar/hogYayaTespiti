71793fb82f8431bca125ccaaac3f24b02e2be653
Serkan <68036775+SerkanBahtiyar@users.noreply.github.com>
2024-03-03 03:59:35 +0300

    HOG ile yaya tespiti

---
+++ yayaTespit/main.py
@@ -0,0 +1,10 @@
+import cv2 as cv
+src=cv.imread("venv/yaya.png")
+hog=cv.HOGDescriptor()
+hog.setSVMDetector(cv.HOGDescriptor_getDefaultPeopleDetector())
+rects,weights=hog.detectMultiScale(src,winStride=(4,4),padding=(8,8),scale=1.25)
+for(x,y,w,h) in rects:
+    cv.rectangle(src,(x,y),(x+w,y+h),(0,255,0),2)
+
+cv.imshow("serkan",src)
+cv.waitKey()
